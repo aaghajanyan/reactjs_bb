1f54df8fff5c9c04c75eeb1995b0dc870f017995
aaghajanyan <albert.aghajanyan.im@gmail.com>
2019-10-16 12:47:05 +0400

    Updated dependencies verson
    	- made code clean up.
    	- commited package-lock.json file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './style.scss';
 
-export default class App extends React.Component {
+class App extends React.Component {
     render () {
         return (
-            <div>ReactJS</div>
+            <div>React JS</div>
         );
     }
 }
+
+export { App };
