2645713b4cdb1dc16430c2083fdd3d2792c7566c
aaghajanyan <albert.aghajanyan.im@gmail.com>
2019-10-30 17:49:39 +0400

    Updated NavBar component and styles based on BEM.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import { NavBar } from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.scss';
 
 class App extends React.Component {
